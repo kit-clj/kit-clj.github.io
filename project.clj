4786ffd34e93ba2eb14b1e4267a416b0fd08ca11
Marko Kocic <marko@euptera.com>
2022-07-28 13:41:47 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://kit-clj.github.io/"
   :license {:name "MIT License"
             :url  "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [hiccup "1.0.5"]
-                 [markdown-clj "1.10.5"]
+                 [markdown-clj "1.11.2"]
                  [crouton "0.1.2"]
                  [luminus/config "0.8"]
-                 [selmer "1.12.44"]
+                 [selmer "1.12.53"]
                  [me.raynes/fs "1.4.6"]]
   :min-lein-version "2.0.0"
   :main kit.core)
